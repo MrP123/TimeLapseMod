362340800405e06f7d18e9dcaa62197e1d7fe27e
MrP123 <mrp.97.3@gmail.com>
2015-06-12 21:19:45 +0200

    initial commit of version 1.0.0

---
+++ config.lua
@@ -0,0 +1,39 @@
+-- ===================================== --
+-- config for Time Lapse Mod (by MrP123) --
+-- ===================================== --
+
+-- disclaimers:
+-- if part of the area of the screenshot isn't loaded yet it will be black
+-- if the area of the game that is on one screenshot is too big everything that is far away from the player will look very bad, this only happens if centerOnPlayerCFG is enabled
+
+-- this is the time difference in seconds between screenshots. This time difference isn't really accurate, as the amount of ticks per second vary depending on if the hardware is able to keep up with the game
+-- default: 300 which equals 5 minutes
+timeDifferenceCFG = 300
+
+-- this is the amount of ticks per second; default: 60 for vanilla Factorio, but if any other mod changes the amount of ticks per second this will have to be changed accordingly or your timing will be off
+ticksPerSecondCFG = 60
+
+-- if this option is enabled all screenshots will be taken at the position of the player; centerOnPlayerCFG and centerOnPositionCFG can't be active at the same time;
+centerOnPlayerCFG = true
+
+-- if this option is enabled all screenshots will be taken at the specified positon; centerOnPlayerCFG and centerOnPositionCFG can't be active at the same time; default: { 0, 0 }
+centerOnPositionCFG = false
+positionCFG = { 0, 0 }	-- if the area at this position isn't loaded the screenshot will be black
+
+-- this is the resolution of the resulting screenshots; default: { 1920, 1080 }
+resolutionCFG = { 1920, 1080 }
+
+-- this is the zoom factor of the resulting screenshots (as a double value > 0); default: 1
+zoomCFG = 1
+
+-- this is the directory inside the script-output directory of the resulting screenshots; the name of the screenshot will be appended to the end like this "script-output/TimeLapseMod/screenshotname.png"; default: "TimeLapseMod"
+pathCFG = "TimeLapseMod"
+
+-- name of the screenshots; default: "screenshot"
+screenshotnameCFG = "screenshot"
+
+-- if enabled the GUI of the game will be visible on the screenshots; default: false
+showguiCFG = false
+
+-- if enabled the Entity Info will be visible on the screenshots; default: false
+showentityinfoCFG = false
---
+++ control.lua
@@ -0,0 +1,52 @@
+require "util"
+require "defines"
+require "config"
+
+game.oninit(function()
+  glob.tickCounter = 0
+  glob.amount = 0
+end)
+
+game.onevent(defines.events.ontick, function(event)
+	glob.tickCounter = glob.tickCounter + 1
+	if glob.tickCounter / (ticksPerSecondCFG * game.speed) > timeDifferenceCFG then	-- this counter is necessary as the lua in Factorio doesn't have access to any libraries, so I can't keep track of time in any other way
+		glob.tickCounter = 0
+		glob.amount = glob.amount + 1
+
+		if centerOnPlayerCFG and centerOnPositionCFG then
+  			game.player.print("ERROR!")
+  			game.player.print("centerOnPlayer and centerOnPositon are enabled at the same time. Please check your config.lua file")
+  			game.player.print("Please change your config.lua file and RESTART Factorio. This message will only be shown ONCE")
+			game.onevent(defines.events.ontick, nil) -- this is done so Factorio unsubscribes from the ontick event as it isn't needed if the screenshot ability doesn't work and therefore improves performance
+  			return
+  		end
+
+  		if not centerOnPlayerCFG and not centerOnPositionCFG then
+  			game.player.print("ERROR!")
+  			game.player.print("centerOnPlayer and centerOnPositon are disabled at the same time. Please check your config.lua file")
+  			game.player.print("Please change your config.lua file and RESTART Factorio. This message will only be shown ONCE")
+			game.onevent(defines.events.ontick, nil) -- this is done so Factorio unsubscribes from the ontick event as it isn't needed if the screenshot ability doesn't work and therefore improves performance
+  			return
+  		end
+
+  		if centerOnPlayerCFG then
+  			local pathName = genPathName(pathCFG, screenshotnameCFG, glob.amount)
+  			game.takescreenshot{resolution = resolutionCFG, zoom = zoomCFG, path = pathName, showguiCFG, showentityinfoCFG}
+  			game.player.print("screenshot taken on player!")
+  		elseif centerOnPositionCFG then
+  			local pathName = genPathName(pathCFG, screenshotnameCFG, glob.amount)
+  			game.takescreenshot{position = positionCFG, resolution = resolutionCFG, zoom = zoomCFG, path = pathName, showguiCFG, showentityinfoCFG}
+  			game.player.print("screenshot taken on positon!")
+  		else
+  			game.player.print("ERROR!")
+  			game.player.print("Something really unexpected/terrible happend. This should never happen!")
+  			game.onevent(defines.events.ontick, nil) -- this is done so Factorio unsubscribes from the ontick event as it isn't needed if something goes wrong majorly and therefore improves performance
+  			return
+  		end
+
+	end
+end)
+
+function genPathName(pathCFG, screenshotnameCFG, amount)
+	return pathCFG .. "/" .. screenshotnameCFG .. amount .. ".png"
+end
